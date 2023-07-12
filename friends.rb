eb49d1e8aa6ea27fb5ec105005ead9a69e7ea42b
Blaine Kennedy <blainekennedy@gmail.com>
2023-07-12 16:48:09 -0500

    Add array and method

--- friends.rb
+++ friends.rb
@@ -2,3 +2,5 @@ arrays = ["Scott", "Mirranda", "Blaine", "Brian", "Steve"]
 puts arrays [2]
 puts arrays [4]
 puts arrays [0]
+places = ["Denver", "Boulder", "Broomfield", "Arvada", "Littleton"]
+puts places.shift
